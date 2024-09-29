86d2600f3d57c2e4b97d33143b876cc82cca7853
Aashish-Aryal <mail.aashisharyal@gmail.com>
2024-09-30 07:14:02 +1000

    cd 7

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,9 @@ set -u # or set -o nounset
 : "$VERSION"
 
 echo "Deploying image: week9task2.azurecr.io/$NAME:$VERSION"
+
+# Update the image tag in the Kubernetes deployment file
+sed -i "s|image: week9task2.azurecr.io/$NAME:.*|image: week9task2.azurecr.io/$NAME:$VERSION|g" ./scripts/kubernetes/$NAME.yaml
+
+# Apply the Kubernetes configuration
 kubectl apply -f ./scripts/kubernetes/$NAME.yaml
